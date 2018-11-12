8066f8ce0c200e830ad59f33a5bb020aa4f0d557
Andrey <vlone96@mail.ru>
2018-11-13 01:47:27 +0300

    test №1

---
+++ js/animation.js
@@ -0,0 +1,17 @@
+document.body.addEventListener('keypress', keyboardHandler);
+
+function keyboardHandler(event) {
+
+    var charCode = event.which || event.keyCode;
+
+    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || // english
+        (charCode > 1024 && charCode < 1106)) { // russian
+        var divElement = document.createElement('div');
+        divElement.className = 'fade-in';
+        divElement.textContent = String.fromCharCode(charCode);
+
+        var lettersInbox = document.querySelector('.lettersInbox');
+        lettersInbox.appendChild(divElement);
+    }
+
+}
