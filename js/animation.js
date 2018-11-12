af3a41bc6b51bee9ab4d73d714cc3e75c20455a0
Shilak-Andrey <44953140+Shilak-Andrey@users.noreply.github.com>
2018-11-13 02:01:14 +0300

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,11 +1,11 @@
-document.body.addEventListener('keypress', keyboardHandler);
+document.body.addEventListener('keypress', presses);
 
-function keyboardHandler(event) {
+function presses(event) {
 
     var charCode = event.which || event.keyCode;
 
-    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || 
-        (charCode > 1024 && charCode < 1106)) { 
+    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || // английские символы
+        (charCode > 1024 && charCode < 1106)) { //русские символы
         var divElement = document.createElement('div');
         divElement.className = 'fade-in';
         divElement.textContent = String.fromCharCode(charCode);
