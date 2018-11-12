af5a0497e0736073bdfdc205113873dbe91fb339
Shilak-Andrey <44953140+Shilak-Andrey@users.noreply.github.com>
2018-11-13 01:59:16 +0300

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -4,8 +4,8 @@ function keyboardHandler(event) {
 
     var charCode = event.which || event.keyCode;
 
-    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || // english
-        (charCode > 1024 && charCode < 1106)) { // russian
+    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || 
+        (charCode > 1024 && charCode < 1106)) { 
         var divElement = document.createElement('div');
         divElement.className = 'fade-in';
         divElement.textContent = String.fromCharCode(charCode);
